170e4902437f50d7bf393d0b9ecac1b097758f0d
Nagashree3007 <crnagashree8@gmail.com>
2024-09-02 11:00:02 +0530

    [Nagashree] Add:UC9-Ability to view Persons by City or State

--- address_book_system.py
+++ address_book_system.py
@@ -4,11 +4,10 @@
 @Date: 29-08-2024
 @Last Modified by: Nagashree C R
 @Last Modified: 29-08-2024
-@Title: UC7-Ability to ensure there is no Duplicate Entry of the same Person in a particular Address Book - Duplicate Check is done
-
+@Title: UC9-Ability to view Persons by City or State
 """
+
 class Contact:
-    # Represents a contact in the address book.
     def __init__(self, first_name, last_name, address, city, state, zip_code, phone_number, email):
         self.first_name = first_name
         self.last_name = last_name
@@ -20,25 +19,12 @@ class Contact:
         self.email = email
 
     def __eq__(self, other):
-        """
-        Overrides the equality operator to compare contacts based on their full name.
-        """
-        if isinstance(other, Contact):
-            return (self.first_name, self.last_name) == (other.first_name, other.last_name)
-        return False
+        return isinstance(other, Contact) and (self.first_name, self.last_name) == (other.first_name, other.last_name)
 
     def __hash__(self):
-        """
-        Overrides the hash function to allow the Contact class to be used in sets and dictionaries.
-        """
         return hash((self.first_name, self.last_name))
 
     def display_contact(self):
-        """
-        Definition: Display the details
-        parameter: None
-        Return: Returns a formatted string of the contact's details.
-        """
         return (f"Name: {self.first_name} {self.last_name}\n"
                 f"Address: {self.address}\n"
                 f"City: {self.city}\n"
@@ -48,11 +34,6 @@ class Contact:
                 f"Email: {self.email}\n")
 
 def get_integer_input(prompt):
-    """
-    Definition: Prompts the user for an integer input 
-    parameter: None
-    Return: returns the integer value.
-    """
     while True:
         try:
             return int(input(prompt))
@@ -60,42 +41,61 @@ def get_integer_input(prompt):
             print("Invalid input. Please enter a valid integer.")
 
 class AddressBook:
-    # Manages contacts in a single address book.
     def __init__(self):
         self.contacts = {}
+        self.city_dict = {}
+        self.state_dict = {}
 
     def add_contact(self, contact):
-        """
-        
-        Definition: Adds a new contact to the address book if it does not already exist.
-        parameter: contact (Contact) - The contact to add.
-        Return: None
-        
-        """
         if contact.first_name in self.contacts:
             existing_contact = self.contacts[contact.first_name]
             if existing_contact == contact:
                 print("Contact already exists.")
                 return
+        
+        self._update_contact_dictionaries(contact)
         self.contacts[contact.first_name] = contact
         print('----------------------------')
         print("Contact added successfully.")
 
-    def update_and_display_contact(self, contact, field_name, new_value):
-        """
-        Updates a specific field of a contact and displays the updated contact details.
-        Definition: Updates a specific field of the contact and prints the updated details.
-        Parameter: contact (Contact) - The contact to update.
-                   field_name (str) - The field to update.
-                   new_value (str or int) - The new value for the field.
-        Return: None
-        """
+    def _update_contact_dictionaries(self, contact):
+        if contact.first_name in self.contacts:
+            old_contact = self.contacts[contact.first_name]
+            if old_contact.city in self.city_dict:
+                self.city_dict[old_contact.city].remove(old_contact)
+            if old_contact.state in self.state_dict:
+                self.state_dict[old_contact.state].remove(old_contact)
+        
+        if contact.city not in self.city_dict:
+            self.city_dict[contact.city] = set()
+        self.city_dict[contact.city].add(contact)
+
+        if contact.state not in self.state_dict:
+            self.state_dict[contact.state] = set()
+        self.state_dict[contact.state].add(contact)
+
+    def update_contact(self, name, field_name, new_value):
+        if name not in self.contacts:
+            print("Contact not found.")
+            return
+        
+        contact = self.contacts[name]
         if field_name == "address":
             contact.address = new_value
         elif field_name == "city":
+            if contact.city in self.city_dict:
+                self.city_dict[contact.city].remove(contact)
             contact.city = new_value
+            if contact.city not in self.city_dict:
+                self.city_dict[contact.city] = set()
+            self.city_dict[contact.city].add(contact)
         elif field_name == "state":
+            if contact.state in self.state_dict:
+                self.state_dict[contact.state].remove(contact)
             contact.state = new_value
+            if contact.state not in self.state_dict:
+                self.state_dict[contact.state] = set()
+            self.state_dict[contact.state].add(contact)
         elif field_name == "zip_code":
             contact.zip_code = new_value
         elif field_name == "phone_number":
@@ -111,88 +111,91 @@ class AddressBook:
         print("-----------------------------------------------------")
 
     def delete_contact(self, name):
-        """
-        Definition: Deletes the contact with the given name.
-        Parameter: name (str) - The name of the contact to delete.
-        Return: None
-        """
         if name in self.contacts:
-            del self.contacts[name]
+            contact = self.contacts.pop(name)
+            if contact.city in self.city_dict:
+                self.city_dict[contact.city].remove(contact)
+                if not self.city_dict[contact.city]:
+                    del self.city_dict[contact.city]
+            if contact.state in self.state_dict:
+                self.state_dict[contact.state].remove(contact)
+                if not self.state_dict[contact.state]:
+                    del self.state_dict[contact.state]
             print(f"Contact '{name}' has been deleted.")
         else:
             print("Contact not found.")
 
     def edit_contact(self, name):
-        """
-        Definition: Edits an existing contact by name.
-        parameter: name (str) - The name of the contact to be edited.
-        Return: None (updates the contact's details).
-        """
-        if not self.contacts:
-            print("No contacts available to edit.")
-            return
-
-        if name in self.contacts:
-            contact = self.contacts[name]
-            while True:
-                print(f"\nEditing contact details for {name}")
-                print("1. Address")
-                print("2. City")
-                print("3. State")
-                print("4. ZIP Code")
-                print("5. Phone Number")
-                print("6. Email")
-                print("7. Exit Editing")
-                choice = input("Enter the number of the detail to edit (1-7): ")
-
-                if choice == "1":
-                    new_address = input("New address: ")
-                    self.update_and_display_contact(contact, "address", new_address)
-                elif choice == "2":
-                    new_city = input("New city: ")
-                    self.update_and_display_contact(contact, "city", new_city)
-                elif choice == "3":
-                    new_state = input("New state: ")
-                    self.update_and_display_contact(contact, "state", new_state)
-                elif choice == "4":
-                    new_zip_code = get_integer_input("New ZIP code: ")
-                    self.update_and_display_contact(contact, "zip_code", new_zip_code)
-                elif choice == "5":
-                    new_phone_number = get_integer_input("New phone number: ")
-                    self.update_and_display_contact(contact, "phone_number", new_phone_number)
-                elif choice == "6":
-                    new_email = input("New email: ")
-                    self.update_and_display_contact(contact, "email", new_email)
-                elif choice == "7":
-                    print("Exiting edit mode.")
-                    break
-                else:
-                    print("Invalid choice. Please try again.")
-        else:
+        if name not in self.contacts:
             print("Contact not found.")
+            return
+        
+        contact = self.contacts[name]
+        while True:
+            print(f"\nEditing contact details for {name}")
+            print("1. Address")
+            print("2. City")
+            print("3. State")
+            print("4. ZIP Code")
+            print("5. Phone Number")
+            print("6. Email")
+            print("7. Exit Editing")
+            choice = input("Enter the number of the detail to edit (1-7): ")
+
+            if choice == "1":
+                new_value = input("New address: ")
+                self.update_contact(name, "address", new_value)
+            elif choice == "2":
+                new_value = input("New city: ")
+                self.update_contact(name, "city", new_value)
+            elif choice == "3":
+                new_value = input("New state: ")
+                self.update_contact(name, "state", new_value)
+            elif choice == "4":
+                new_value = get_integer_input("New ZIP code: ")
+                self.update_contact(name, "zip_code", new_value)
+            elif choice == "5":
+                new_value = get_integer_input("New phone number: ")
+                self.update_contact(name, "phone_number", new_value)
+            elif choice == "6":
+                new_value = input("New email: ")
+                self.update_contact(name, "email", new_value)
+            elif choice == "7":
+                print("Exiting edit mode.")
+                break
+            else:
+                print("Invalid choice. Please try again.")
 
     def display_contacts(self):
-        """
-        Definition: Displays all contacts in the address book.
-        Parameter: None
-        Return: None
-        """
         if not self.contacts:
             print("No contacts available.")
         else:
             for contact in self.contacts.values():
                 print(contact.display_contact())
 
+    def view_contacts_by_city(self, city):
+        contacts = self.city_dict.get(city, None)
+        if contacts:
+            print(f"Contacts in {city}:")
+            for contact in contacts:
+                print(contact.display_contact())
+        else:
+            print(f"No contacts found in {city}.")
+
+    def view_contacts_by_state(self, state):
+        contacts = self.state_dict.get(state, None)
+        if contacts:
+            print(f"Contacts in {state}:")
+            for contact in contacts:
+                print(contact.display_contact())
+        else:
+            print(f"No contacts found in {state}.")
+
 class AddressBookManager:
     def __init__(self):
         self.address_books = {}
 
     def add_address_book(self, name):
-        """
-        Definition: Adds a new address book with the given name.
-        Parameter: name (str) - The name of the address book.
-        Return: None
-        """
         if name in self.address_books:
             print("Address book with this name already exists.")
         else:
@@ -200,19 +203,11 @@ class AddressBookManager:
             print(f"Address book '{name}' added successfully.")
 
     def get_address_book(self, name):
-        """
-        Definition: Retrieves the address book with the given name.
-        Parameter: name (str) - The name of the address book.
-        Return: AddressBook - The address book associated with the name.
-        """
         return self.address_books.get(name, None)
 
 def main_menu(manager):
-    """
-    Definition: Displays options to the user and processes their choice.
-    Parameter: manager (AddressBookManager) - The address book manager.
-    Return: None
-    """
+    selected_address_book = None
+    
     while True:
         print("-------------------------------------------------\n")
         print("1. Add Address Book")
@@ -237,9 +232,9 @@ def main_menu(manager):
 
             if 0 <= index < len(names):
                 selected_name = names[index]
-                address_book = manager.get_address_book(selected_name)
-                if address_book:
-                    address_book_menu(address_book)
+                selected_address_book = manager.get_address_book(selected_name)
+                print(f"Selected Address Book: {selected_name}")
+                address_book_menu(selected_address_book)
             else:
                 print("Invalid selection. Please try again.")
         elif choice == "3":
@@ -249,18 +244,15 @@ def main_menu(manager):
             print("Invalid choice. Please try again.")
 
 def address_book_menu(address_book):
-    """
-    Definition: Displays options to manage the selected address book.
-    Parameter: address_book (AddressBook) - The selected address book.
-    Return: None
-    """
     while True:
         print("-------------------------------------------------\n")
         print("1. Add Contact")
         print("2. Edit Contact")
         print("3. Delete Contact")
         print("4. Display Contacts")
-        print("5. Exit")
+        print("5. View Contacts by City")
+        print("6. View Contacts by State")
+        print("7. Exit")
         choice = input("Enter your choice: ")
 
         if choice == "1":
@@ -288,20 +280,17 @@ def address_book_menu(address_book):
 
                 address_book.add_contact(contact)
 
-                # Check if the user wants to add another contact
                 another = input("Do you want to add another contact? (yes/no): ").strip().lower()
                 if another != 'yes':
                     break
         elif choice == "2":
             if not address_book.contacts:
-                print('--------------------------------')
                 print("No contacts available to edit.")
             else:
                 name = input("Enter the name of the contact to edit: ")
                 address_book.edit_contact(name)
         elif choice == "3":
             if not address_book.contacts:
-                print('--------------------------------')
                 print("No contacts available to delete.")
             else:
                 name = input("Enter the name of the contact to delete: ")
@@ -310,11 +299,17 @@ def address_book_menu(address_book):
             print("\n-----------------Contacts in Address Book:------------")
             address_book.display_contacts()
         elif choice == "5":
-            print("-------------------Exiting address book.------------------")
+            city = input("Enter city to view contacts: ")
+            address_book.view_contacts_by_city(city)
+        elif choice == "6":
+            state = input("Enter state to view contacts: ")
+            address_book.view_contacts_by_state(state)
+        elif choice == "7":
+            print("Exiting address book menu.")
             break
         else:
             print("Invalid choice. Please try again.")
 
-# Initialize AddressBookManager and start main menu
-address_book_manager = AddressBookManager()
-main_menu(address_book_manager)
+if __name__ == "__main__":
+    manager = AddressBookManager()
+    main_menu(manager)
